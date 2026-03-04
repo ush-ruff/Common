7f2fdeaac2139698720dbb9c3bf2887df02dc9da
ush-ruff <85804455+ush-ruff@users.noreply.github.com>
2026-03-04 23:17:40 +0500

    Update downloadURL to main branch

--- Userscript-Helper-Lib/helpersLib.user.js
+++ Userscript-Helper-Lib/helpersLib.user.js
@@ -6,7 +6,7 @@
 // @match        *://*/*
 // @icon
 // @homepageURL  https://github.com/ush-ruff/Common/
-// @downloadURL  https://github.com/ush-ruff/Common/raw/standalone-version/Userscript-Helper-Lib/helpersLib.user.js
+// @downloadURL  https://github.com/ush-ruff/Common/raw/main/Userscript-Helper-Lib/helpersLib.user.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
